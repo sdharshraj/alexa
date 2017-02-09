e7a231bcd83ee2781b418192348897df3f8090fe
Harsh Kumar (IN6076) <M1036076@mindtree.com>
2017-02-10 01:16:34 +0530

    asa

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+app.set('view engine', 'html');
 
+app.engine('.html', require('ejs').renderFile);
+app.get('/',function(req,res){
+  res.render('index');
+})
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        console.log( data );
